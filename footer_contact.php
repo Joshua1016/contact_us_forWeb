8b55843a419452f932c1c17d5b5cccc34f971e06
Joshua1016 <150825264+Joshua1016@users.noreply.github.com>
2024-01-24 09:30:53 +0800

    Add files via upload

---
+++ footer_contact.php
@@ -0,0 +1,109 @@
+ <!-- ======= Footer ======= -->
+ <footer id="footer" class="footer">
+
+<div class="footer-content">
+  <div class="container">
+    <div class="row gy-4">
+      <div class="col-lg-5 col-md-12 footer-info">
+        <a href="index.php" class="logo d-flex align-items-center">
+          <h2
+            style="color: #fdd700; text-shadow: -1px -1px 2px rgba(0, 0, 0, 0.5), 1px -1px 2px rgba(0, 0, 0, 0.5), -1px 1px 2px rgba(0, 0, 0, 0.5), 1px 1px 2px rgba(0, 0, 0, 0.5);">
+            Welcome to First Farmers Holding Corporation!</h2>
+        </a>
+        <p style="color: white;">After a period of hibernation, the First Farmers Holding Corporation (FFHC) website
+          is back and brandishes a new look!.</p>
+        <div class="social-links d-flex  mt-3">
+          <!-- <a href="#" class="twitter"><i class="bi bi-twitter"></i></a> -->
+          <a href="https://www.facebook.com/search/top?q=first%20farmers%20holding%20corporation" class="facebook"><i class="bi bi-facebook"></i></a>
+          <!-- <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+          <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a> -->
+        </div>
+      </div>
+
+      <div class="col-lg-2 col-6 footer-links">
+        <h4 style="color: gold;">Useful Links</h4>
+        <ul>
+          <li><i class="bi bi-dash" style="color: #fdd700;"></i> <a href="../index.php" style="color: white;">Home</a>
+          </li>
+          <li><i class="bi bi-dash" style="color: #fdd700;"></i> <a href="../pages/about.php" style="color: white;">About
+              us</a></li>
+          <li><i class="bi bi-dash" style="color: #fdd700;"></i> <a href="../pages/services.php"
+              style="color: white;">Services & Policy</a></li>
+
+          <li><i class="bi bi-dash" style="color: #fdd700;"></i> <a href="email.php" style="color: white;">Contact
+              us</a>
+        </ul>
+      </div>
+
+
+
+
+
+      <div class="col-lg-2 col-6 footer-links">
+        <h4 style="color: gold;">Our Services</h4>
+        <ul>
+          <li><i class="bi bi-dash" style="color: #fdd700;"></i> <a href="/pages/services.php" style="color: white;">Raw
+              Sugar</a>
+          </li>
+          <li><i class="bi bi-dash" style="color: #fdd700;"></i> <a href="/pages/services.php"
+              style="color: white;">Refined Sugar</a></li>
+          <li><i class="bi bi-dash" style="color: #fdd700;"></i> <a href="/pages/services.php"
+              style="color: white;">Molasses</a></li>
+          <li><i class="bi bi-dash" style="color: #fdd700;"></i> <a href="/pages/services.php"
+              style="color: white;">Power</a>
+          </li>
+        </ul>
+      </div>
+
+
+      <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
+        <h4 style="color: gold;">Contact Us</h4>
+        <p style="color: white;">
+          Brgy. Dos Hermanas <br>
+          Talisay City, 6115<br>
+          Negros Occidental <br><br>
+          <strong>Phone:</strong> : (034) 441-0255<br>
+          <strong>Email:</strong> : firstfarmershrd@gmail.com<br>
+        </p>
+
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<div class="footer-legal">
+  <div class="container">
+    <div class="copyright">
+      &copy; Copyright <strong><span>2023</span></strong>. First Farmers Holding Corporation
+    </div>
+    <div class="credits">
+      <!-- All the links in the footer should remain intact. -->
+      <!-- You can delete the links only if you purchased the pro version. -->
+      <!-- Licensing information: https://bootstrapmade.com/license/ -->
+      <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nova-bootstrap-business-template/ -->
+      Designed by: <a href="https://www.facebook.com/joeymune" style="color: gold; text-decoration: underline;">Joshua Muñeza</a>
+    </div>
+  </div>
+</div>
+</footer><!-- End Footer --><!-- End Footer -->
+
+<a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i
+  class="bi bi-arrow-up-short"></i></a>
+
+<div id="preloader"></div>
+
+<!-- Vendor JS Files -->
+<script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../assets/vendor/aos/aos.js"></script>
+<script src="../assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="../assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<!-- <script src="assets/vendor/php-email-form/validate.js"></script> -->
+
+<!-- Template Main JS File -->
+<script src="../assets/js/main.js"></script>
+
+</body>
+
+</html>
